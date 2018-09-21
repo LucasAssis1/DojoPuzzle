3dd7bb6db20c7eda4326e8924691493783a7399e
Lucas Assis <lucas.assis@programmers.com.br>
2018-09-21 15:31:53 -0300

    initial commit

---
+++ Tests/DojoPuzzle.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DojoPuzzle.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Should_return_234_written()
+        {
+            
+        }
+    }
+}
